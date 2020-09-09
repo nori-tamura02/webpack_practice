ea191c6910b14b53fecfb354375103e83ed8f711
nori-tamura02 <n0r1.ta6ra@gmail.com>
2020-09-09 12:30:54 +0900

    Extract CSS and HTML
    the commit message for your changes. Lines starting

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPligin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -12,7 +14,7 @@ module.exports = {
                 test: /\.css/,
                 use: [
                     {
-                        loader: 'style-loader',
+                        loader: MiniCssExtractPligin.loader,
                     },
                     {
                         loader: 'css-loader',
@@ -21,4 +23,10 @@ module.exports = {
             },
         ],
     },
+    plugins: [
+        new MiniCssExtractPligin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
 }
